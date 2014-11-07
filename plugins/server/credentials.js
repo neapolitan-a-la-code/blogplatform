d13efdd98a928b232f197e9f76be730072d4b8cc
NataliaLKB <natalialeebaltazar@gmail.com>
2014-11-07 13:18:35 +0000

    push to heroku

---
+++ plugins/server/credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+	facebook: {
+		clientId: '662892673818088',
+		clientSecret: 'e040d3e086d50d69f0707ab290ed3da2'
+	},
+	google: {
+		clientId: '774125461948-ahog0a1riq77aoovpvpatbd9pbn8qmai.apps.googleusercontent.com',
+		clientSecret: 'BRnnbpHcfOb8JJ0TIhx9ak5A'
+	}
+};
